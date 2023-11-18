a424e77c1760cd6f7e6408c6272f81c1af60db7d
kauan junker <kauanjunker@gmail.com>
2023-11-18 19:23:44 -0300

    countdown and offer

--- components/countDown.tsx
+++ components/countDown.tsx
@@ -1,10 +1,11 @@
 'use client'
 import Countdown from "react-countdown"
-
 const endingDate = new Date("2023-12-01")
 
 export function CountDown(){
     return(
-        <Countdown className="font-bold text-5xl text-yellow-300" date={endingDate}/>
+        <>
+        <Countdown className="font-bold text-5xl text-yellow-300" date={endingDate}/>    
+        </>
     )
 }
--- components/offer.tsx
+++ components/offer.tsx
@@ -8,13 +8,17 @@ export function Offer(){
             {/* TEXT CONTAINER */}
             <div className="flex-1 flex flex-col items-center justify-center 
             text-center gap-8 p-6">
-                <h1 className="text-white text-5xl font-bold xl:text-6xl">Delicious Burger & French Fryc</h1>
+                <h1 className="text-white text-5xl font-bold xl:text-6xl">
+                    Delicious Burger & French Fryc
+                </h1>
                 <p className="text-white xl:text-xl">
-                Progressively simplify effective e-toilers and process-centric methods
-                of empowerment. Quickly pontificate parallel.
+                Progressively simplify effective e-toilers and process-centric 
+                methods of empowerment. Quickly pontificate parallel.
                 </p>
                 <CountDown/>
-                <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
+                <button className="bg-red-500 text-white rounded-md py-3 px-6">
+                    Order Now
+                </button>
             </div>
 
             {/* IMAGE CONTAINER */}
