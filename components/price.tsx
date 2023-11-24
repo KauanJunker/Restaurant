8cebceed5de2814b6717cfe4513cf782d2fbce80
kauan junker <kauanjunker@gmail.com>
2023-11-23 22:39:45 -0300

    cart component

--- components/price.tsx
+++ components/price.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import { ProductType } from "@/types/types";
+import { useCartStore } from "@/utils/store";
 import { useEffect, useState } from "react";
-
+import { toast } from "react-toastify";
 
 export function Price({ product }: { product: ProductType}){
   const [total, setTotal] = useState(product.price)
   const [quantity, setQuantity] = useState(1)
   const [selected, setSelected] = useState(0)
+
+  const { addtoCart } = useCartStore()
 
   useEffect(() => {
     if(product.options?.length) {
@@ -16,6 +19,20 @@ export function Price({ product }: { product: ProductType}){
       )
     }
   }, [quantity, selected, product])
+
+  function handleCart() {
+    addtoCart({
+      id: product.id,
+      title: product.title,
+      img: product.img,
+      price: total,
+      ...(product.options?.length && {
+        optionTitle: product.options[selected].title
+      }),
+      quantity: quantity,
+     })
+     toast.success("The product added to the cart")
+  }
 
     return(
         <div className="flex flex-col gap-4">
@@ -56,7 +73,9 @@ export function Price({ product }: { product: ProductType}){
               </div>
             </div>
             {/* CART BUTTON*/}
-            <button className="uppercase w-56 bg-red-500 text-white p-3
+            <button 
+             onClick={handleCart}
+             className="uppercase w-56 bg-red-500 text-white p-3
             ring-1 ring-red-500">
               Add to Cart
             </button>
